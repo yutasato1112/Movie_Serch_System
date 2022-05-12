288f0ca551584c34353a41adccebc46e7c30f078
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2022-05-12 12:27:53 +0900

    add head

---
+++ index.php
@@ -0,0 +1,6 @@
+<HTML>
+<HEAD>
+<TITLE>ジャンプ</TITLE>
+<META http-equiv="Refresh" content="1;URL=php/home.php">
+</HEAD>
+</HTML>
---
+++ php/result.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<header>
+    <meta name="description" content="燃料代計算">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/css_home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Luxurious+Roman&display=swap" rel="stylesheet">
+    <title>検索結果</title>
+</header>
+
+
+<body>
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <!--ここから-->
+    
+    <!--ここまで-->
+<body>
